fc3459105c7c8b90093cfdb55cc8827bac52bb84
Felipe Rocha <feliperocha.ti@gmail.com>
2021-02-12 23:20:47 +0100

    update Game tests

--- src/test/java/net/feliperocha/gameofthree/unit/GameUnitTest.java
+++ src/test/java/net/feliperocha/gameofthree/unit/GameUnitTest.java
@@ -18,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class GameUnitTest {
     private final static Integer DIVISOR = 3;
     private final static Integer WINNING_NUMBER = 1;
+    private final static Integer INITIAL_NUMBER = 56;
     private final static Player FIRST_PLAYER = new Player("First", true);
     private final static Player SECOND_PLAYER = new Player("Second", false);;
     private Game GAME;
@@ -25,8 +26,27 @@ public class GameUnitTest {
     @BeforeAll
     void create_test_game() {
         GAME = new Game();
-        GAME.setInitialNumber(56);
+        GAME.setInitialNumber(INITIAL_NUMBER);
         GAME.getPlayers().addAll(List.of(FIRST_PLAYER, SECOND_PLAYER));
+    }
+
+    @Test
+    void execute_moves_until_game_end() {
+        //given
+        FIRST_PLAYER.setStatus(PLAYING);
+        SECOND_PLAYER.setStatus(PLAYING);
+
+        //when
+        GAME.executeMove(new MoveDTO(GAME.getId(), SECOND_PLAYER.getId(), ADD), DIVISOR, WINNING_NUMBER);
+        GAME.executeMove(new MoveDTO(GAME.getId(), FIRST_PLAYER.getId(), SUBTRACT), DIVISOR, WINNING_NUMBER);
+        GAME.executeMove(new MoveDTO(GAME.getId(), SECOND_PLAYER.getId(), MAINTAIN), DIVISOR, WINNING_NUMBER);
+        GAME.executeMove(new MoveDTO(GAME.getId(), FIRST_PLAYER.getId(), ADD), DIVISOR, WINNING_NUMBER);
+
+        //then
+        assertTrue(GAME.getPlayers().stream().allMatch(p -> p.getStatus().equals(PlayerStatus.FINISHED)));
+        assertEquals(GAME.getStatus(), FINISHED);
+        assertEquals(GAME.getMoves().stream().max(comparing(Move::getCreatedAt)).get().getCurrentNumber(), WINNING_NUMBER);
+        assertEquals(GAME.getWinnerPlayer(), Optional.of(FIRST_PLAYER));
     }
 
     @Test
@@ -67,25 +87,6 @@ public class GameUnitTest {
     void get_winner_player() {
         assertEquals(GAME.getWinnerPlayer(), Optional.empty());
         GAME.setWinnerPlayerId(Optional.of(FIRST_PLAYER.getId()));
-        assertEquals(GAME.getWinnerPlayer(), Optional.of(FIRST_PLAYER));
-    }
-
-    @Test
-    void execute_moves_until_game_end() {
-        //given
-        FIRST_PLAYER.setStatus(PLAYING);
-        SECOND_PLAYER.setStatus(PLAYING);
-
-        //when
-        GAME.executeMove(new MoveDTO(GAME.getId(), SECOND_PLAYER.getId(), ADD), DIVISOR, WINNING_NUMBER);
-        GAME.executeMove(new MoveDTO(GAME.getId(), FIRST_PLAYER.getId(), SUBTRACT), DIVISOR, WINNING_NUMBER);
-        GAME.executeMove(new MoveDTO(GAME.getId(), SECOND_PLAYER.getId(), MAINTAIN), DIVISOR, WINNING_NUMBER);
-        GAME.executeMove(new MoveDTO(GAME.getId(), FIRST_PLAYER.getId(), ADD), DIVISOR, WINNING_NUMBER);
-
-        //then
-        assertTrue(GAME.getPlayers().stream().allMatch(p -> p.getStatus().equals(PlayerStatus.FINISHED)));
-        assertEquals(GAME.getStatus(), FINISHED);
-        assertEquals(GAME.getMoves().stream().max(comparing(Move::getCreatedAt)).get().getCurrentNumber(), WINNING_NUMBER);
         assertEquals(GAME.getWinnerPlayer(), Optional.of(FIRST_PLAYER));
     }
 }
